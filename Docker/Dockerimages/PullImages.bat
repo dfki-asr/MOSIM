623ab4c5d460323a28d6c0d532729a8a1362c81c
Janis Sprenger <Janis.Sprenger@dfki.de>
2022-03-23 18:12:27 +0100

    Moved docker files from old repository

---
+++ Docker/Dockerimages/PullImages.bat
@@ -0,0 +1,15 @@
+
+docker pull mosim01/launcher:latest
+
+docker pull mosim01/csharpadapter:latest 
+
+docker pull mosim01/unity:unityadapter
+
+docker pull mosim01/postureblending:latest
+
+docker pull mosim01/coordinatesystemmapper:latest
+
+docker pull mosim01/unity:pathplanning
+
+docker pull mosim01/blenderservice:latest
+
