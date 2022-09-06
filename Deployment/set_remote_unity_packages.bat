7bf03a833fdbf68ee91da0215f277613bc51fca8
Janis Sprenger <janis.sprenger@dfki.de>
2022-09-06 11:07:08 +0200

    Updated setter for remote unity packages

--- Deployment/set_remote_unity_packages.bat
+++ Deployment/set_remote_unity_packages.bat
@@ -1,30 +1,36 @@
 @echo off
-
 REM Set absolute path to meta repository
 SET HOME2=%~dp0\..
 FOR /F %%i IN ("%HOME2%") DO SET "HOME=%%~fi"
 
 REM get current branch
-cd %HOME%\Repos\MOSIM-Unity
-set current_branch=
-for /F "delims=" %%n in ('git branch --show-current') do set "current_branch=%%n"
-if "%current_branch%"=="" echo Not a git branch! && goto :EOF
+REM cd %HOME%\Repos\MOSIM-Unity
 
+if "%1"=="" (
+	set current_branch=
+	for /F "delims=" %%n in ('git branch --show-current') do set "current_branch=%%n"
+	if "%current_branch%"=="" echo  && goto :EOF
+) else (
+	set current_branch=%1
+)
 
-if "%current_branch%"=="master" DO (
-	SET REPLACE1= https://github.com/dfki-asr/MMIUnity-Core.git
-	SET REPLACE2= https://github.com/dfki-asr/MMIUnity-TargetEngine.git
+if %Current_Branch%==master (
+	SET REPLACE1=https://github.com/dfki-asr/MMIUnity-Core.git
+	SET REPLACE2=https://github.com/dfki-asr/MMIUnity-TargetEngine.git
 ) else (
 	SET REPLACE1= https://github.com/dfki-asr/MMIUnity-Core.git#%Current_Branch%
 	SET REPLACE2= https://github.com/dfki-asr/MMIUnity-TargetEngine.git#%Current_Branch%
 )
 
-cd %HOME%\Deployment
+REM cd %HOME%\Deployment
 
 
 ECHO "This batch file changes the manifest.json."
 ECHO "It will result in the git packages being used as the Unity packages."
 echo "The current branch of MOSIM-Unity is %current_branch%"
+echo "The following branches will be set: "
+echo "    %REPLACE1%
+echo "    %REPLACE2%
 ECHO "                        "
 ECHO " ---------------------- "
 ECHO "        _   __ ___      "
