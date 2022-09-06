06cb4e5b7ffe0b455e9ec8409b17e5c522bf01e3
Janis Sprenger <janis.sprenger@dfki.de>
2022-09-06 09:20:50 +0200

    AJAN template renaming
    - As template is a reserved name in C++, it was changed to templateAgent
    - distributed generated code was updated with the correct python path

--- Framework-Definition/distributedGeneratedCode.bat
+++ Framework-Definition/distributedGeneratedCode.bat
@@ -2,11 +2,11 @@
 setlocal
 
 REM C# Code distribution
-xcopy /Y %~dp0\gen-csharp %~dp0\..\Repos\Packages\MMICSharp-Core\MMIStandard
+xcopy /Y/S/E %~dp0\gen-csharp %~dp0\..\Repos\Packages\MMICSharp-Core\MMIStandard
 
 REM python copy
-xcopy /Y/S/E %~dp0\gen-py\MMIStandard %~dp0\..\Repos\Packages\MMIPython-Core\src\MOSIM\mmi
+xcopy /Y/S/E/D %~dp0\gen-py\MMIStandard %~dp0\..\Repos\Packages\MMIPython-Core\src\MMI-Standard\
 
 REM copy Cpp generated code
-xcopy /Y %~dp0\gen-cpp\*.h %~dp0\..\Repos\Packages\MMI-UnrealPlugin\Source\MMMIScene\Public\gen-cpp
-xcopy /Y %~dp0\gen-cpp\*.cpp %~dp0\..\Repos\Packages\MMI-UnrealPlugin\Source\MMMIScene\Private\gen-cpp
+xcopy /Y/S/E %~dp0\gen-cpp\*.h %~dp0\..\Repos\Packages\MMI-UnrealPlugin\Source\MMMIScene\Public\gen-cpp
+xcopy /Y/S/E %~dp0\gen-cpp\*.cpp %~dp0\..\Repos\Packages\MMI-UnrealPlugin\Source\MMMIScene\Private\gen-cpp
--- Framework-Definition/mmi/AJAN.thrift
+++ Framework-Definition/mmi/AJAN.thrift
@@ -10,7 +10,7 @@ struct MRDFGraph {
 }
 
 service MAJANService {
-	string CreateAgent(1:string name, 2:string template, 3:MRDFGraph knowledge),
+	string CreateAgent(1:string name, 2:string templateAgent, 3:MRDFGraph knowledge),
 	bool DeleteAgent(1:string agentName),
 	string ExecuteAgent(1:string agentName, 2:string endpoint, 3:MRDFGraph content),
   void ReceiveAsync(1:string actionID, 2:i32 answer)
