e45dadc63834c465614e68a336718309f04dd595
Niklas Braun <niklas.braun@dfki.de>
2022-10-10 12:24:50 +0200

    Changed .bat to only relative paths for the manifest. This mitgates errors where developers might push manifest files with their local machine paths which will not work on other machines.

--- Deployment/set_local_unity_packages.bat
+++ Deployment/set_local_unity_packages.bat
@@ -4,8 +4,8 @@ REM Set absolute path to meta repository
 SET HOME2=%~dp0\..
 FOR /F %%i IN ("%HOME2%") DO SET "HOME=%%~fi"
 
-SET REPLACE1= file:%HOME%/Repos/Packages/MMIUnity-Core
-SET REPLACE2= file:%HOME%/Repos/Packages/MMIUnity-TargetEngine
+SET REPLACE1=/Packages/MMIUnity-Core
+SET REPLACE2=/Packages/MMIUnity-TargetEngine
 
 ECHO "This batch file changes the manifest.json."
 ECHO "It will result in the local packages being used as the Unity packages."
@@ -19,16 +19,16 @@ ECHO " ---------------------- "
 ECHO.
 
 SET MANIFEST= %HOME%\Repos\MOSIM-Unity\Demos\SimpleUnityDemo\Packages\manifest.json
-python Scripts\String-replace.py %MANIFEST% de.dfki.mmiunity-core %REPLACE1%
-python Scripts\String-replace.py %MANIFEST% de.dfki.mmiunity-targetengine %REPLACE2%
+python Scripts\String-replace.py %MANIFEST% de.dfki.mmiunity-core file:..\..\..\..%REPLACE1%
+python Scripts\String-replace.py %MANIFEST% de.dfki.mmiunity-targetengine file:..\..\..\..%REPLACE2%
 
 SET MANIFEST= %HOME%\Repos\MOSIM-Unity\Adapter\MMIAdapterUnity\Packages\manifest.json
-python Scripts\String-replace.py %MANIFEST% de.dfki.mmiunity-core %REPLACE1%
+python Scripts\String-replace.py %MANIFEST% de.dfki.mmiunity-core file:..\..\..\..%REPLACE1%
 
 SET MANIFEST= %HOME%\Repos\MOSIM-Unity\Services\UnityPathPlanning\UnityPathPlanningService\Packages\manifest.json
-python Scripts\String-replace.py %MANIFEST% de.dfki.mmiunity-core %REPLACE1%
+python Scripts\String-replace.py %MANIFEST% de.dfki.mmiunity-core file:..\..\..\..\..%REPLACE1%
 
 SET MANIFEST= %~dp0..\Repos\MOSIM-Unity\Tools\SkeletonConfigurator\Packages\manifest.json
-python Scripts\String-replace.py %MANIFEST% de.dfki.mmiunity-core %REPLACE1%
+python Scripts\String-replace.py %MANIFEST% de.dfki.mmiunity-core file:..\..\..\..%REPLACE1%
 
 pause
